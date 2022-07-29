b3a7ffb7830145dc0af76dda474ba8439b890e10
nishantshekhar1 <nishnat.390@gmail.com>
2022-07-29 17:43:08 -0400

    changes

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // App Settings
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/newdb',
-  TOKEN_SECRET: process.env.TOKEN_SECRET || 'YOUR_UNIQUE_JWT_TOKEN_SECRET',
+  MONGO_URI: process.env.MONGO_URI,
+  TOKEN_SECRET: process.env.TOKEN_SECRET,
 
   // OAuth 2.0
   FACEBOOK_SECRET: process.env.FACEBOOK_SECRET,
